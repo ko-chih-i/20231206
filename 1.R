3559d2795e921b0b998234d45b39b02b85557812
le402n54\uer <julia910527@gmail.com>
2023-12-06 16:53:58 +0800

    擷取ppt資訊

--- 1.R
+++ 1.R
@@ -42,3 +42,13 @@ sub_website = read_html(sub_link)
 
 article_info = sub_website %>% html_nodes(".article-meta-value")
 article_info
+URL = "https://www.ptt.cc/bbs/AllTogether/index3245.html"
+website = read_html(URL)
+
+needed_html = website %>% html_nodes("a")
+needed_html
+needed_txt = needed_html %>% html_text()
+needed_txt
+intrested_pos = grep("[徵女]", needed_txt, fixed = TRUE)
+needed_txt[intrested_pos]
+needed_link = needed_html[intrested_pos] %>% html_attr("href")
